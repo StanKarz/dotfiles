55e43123be6964a5d777f0be4ecd793b359641d1
Stan <60108739+StanKarz@users.noreply.github.com>
2025-01-09 23:20:51 +0000

    Added git config & updated install script

--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@
 # Ensure the .config directory exists
 mkdir -p ~/.config
 
+# Symlink Git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
 # Symlink starship.toml
 ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
 
-# Symlink other files
+# Symlink zsh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Symlink tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 echo "Dotfiles installed successfully!"
